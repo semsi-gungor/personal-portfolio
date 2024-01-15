24086bebc1fccc171412d508a54345a254ec54cc
semsi-gungor <semsi.gungor00@gmail.com>
2024-01-15 14:09:42 +0300

    feat: add onviewtrigger component

---
+++ src/components/UI/OnViewTrigger/index.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { FC, useEffect } from "react";
+import styles from "./styles.module.scss";
+import { useInView } from "react-intersection-observer";
+import { useNavbar } from "@/context/navbar-context";
+
+interface OnViewTriggerProps {
+  index: number;
+}
+
+const OnViewTrigger: FC<OnViewTriggerProps> = ({ index }) => {
+  const { setNavbarIndex } = useNavbar();
+
+  const { ref, inView, entry } = useInView({ threshold: 0.8 });
+
+  useEffect(() => {
+    if (inView) setNavbarIndex(index);
+  }, [inView]);
+
+  return <div ref={ref} className={styles.trigger} aria-hidden></div>;
+};
+
+export default OnViewTrigger;
