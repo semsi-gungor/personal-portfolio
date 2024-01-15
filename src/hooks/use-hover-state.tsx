d2095ec8aa379c71a29d227bb53dacc96c5c9538
semsi-gungor <semsi.gungor00@gmail.com>
2024-01-15 15:20:53 +0300

    feat: add context for cursor

---
+++ src/hooks/use-hover-state.tsx
@@ -0,0 +1,28 @@
+import { useCursor } from "@/context/mouse-context";
+import { useRef, useEffect } from "react";
+
+export function useHoverState<T extends HTMLElement>(type: "hover" | "link") {
+  const ref = useRef<T>(null);
+  const { setCursorState } = useCursor();
+
+  useEffect(() => {
+    if (!ref.current) return;
+
+    function onMouseEnter() {
+      setCursorState(type);
+    }
+    ref.current.addEventListener("mouseenter", onMouseEnter);
+
+    function onMouseLeave() {
+      setCursorState("default");
+    }
+    ref.current.addEventListener("mouseleave", onMouseLeave);
+
+    return () => {
+      ref.current?.removeEventListener("mouseenter", onMouseEnter);
+      ref.current?.removeEventListener("mouseleave", onMouseLeave);
+    };
+  }, [ref]);
+
+  return ref;
+}
