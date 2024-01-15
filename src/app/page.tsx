091e1159f5507b9abb3708863c808e5b86869c3e
semsi-gungor <semsi.gungor00@gmail.com>
2024-01-15 15:03:49 +0300

    chore: update main page with new sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import About from "@/components/About";
+import Contact from "@/components/Contact";
 import Home from "@/components/Home";
+import Works from "@/components/Works";
 
 export default function Page() {
   return (
     <main>
       <Home />
+      <About />
+      <Works />
+      <Contact />
     </main>
   );
 }
